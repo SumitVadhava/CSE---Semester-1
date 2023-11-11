947634565bd4c6e83a03d32050973fec2e4db660
Sumit  Vadhava <140806339+SumitVadhava@users.noreply.github.com>
2023-11-11 14:27:35 +0530

    Add files via upload

---
+++ Lab G-305/Lab 04/B/4-B(1).cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	float h,b;
+	printf("Enter Base Of Triangle=");
+	scanf("%f",&b);
+	printf("Enter Height Of Triangle=");
+	scanf("%f",&h);
+	printf("Area=%f",(h*b)/2);
+	return 0;
+}
